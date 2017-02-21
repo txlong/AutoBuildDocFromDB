c99f77aa13a1bcd7a75f7c78820b029e028702b0
xuanskyer <xuanskyer@163.com>
2017-02-22 00:03:12 +0800

    添加数据表页面跳转锚点

--- MarkdownBuildClass.py
+++ MarkdownBuildClass.py
@@ -9,8 +9,13 @@ class MarkDownBuild:
 
     def buildMarkdown(self, table_data):
         text = '# 数据库文档\n\n'
+        text += '<span id="返回顶部"></span>\n\n## 数据表列表\n\n'
         for table in table_data:
-            text = text + '## ' + table[0][0] + '表(' + table[0][1] + ')\n'
+            text = text + '* [' + table[0][0] + '(' + table[0][1] + ')](#' + table[0][0] + '_pointer)\n'
+        text += '## 数据表说明\n\n'
+        for table in table_data:
+            text = text + '<span id="'+table[0][0]+'_pointer"></span>\n\n'
+            text = text + '* ' + table[0][0] + '表(' + table[0][1] + ')[↑](#返回顶部)\n'
             text += self.table_header
             for column in table[1]:
                 text = text + '|' + column[0] + '|' + column[1] + '|' + column[2] + '|\n'
